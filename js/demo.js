da1cbcda342f7a92567f9e297644e68120fd9a9d
TM studios <154602924+TM-studios@users.noreply.github.com>
2026-01-07 16:47:52 +0100

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,15 @@
+const validCodes = ["FINENCIALBETA261", "FINENCIALBETA262"];
+
+const input = document.getElementById("betaCode");
+const button = document.getElementById("enterBtn");
+const errorMsg = document.getElementById("errorMsg");
+
+button.addEventListener("click", () => {
+    const enteredCode = input.value.trim();
+
+    if (validCodes.includes(enteredCode)) {
+        window.location.href = "html/nav-link/homepage.html";
+    } else {
+        errorMsg.textContent = "Codice beta non valido";
+    }
+});
